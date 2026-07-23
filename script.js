3b9b397a4c030e84ce1d5c40063f375168885efb
Timal23 <marwin.verin@gmail.com>
2026-07-23 22:40:55 +0200

    maj

--- script.js
+++ script.js
@@ -132,7 +132,6 @@ function afficherMain(joueur, conteneur) {
     const spread       = Math.min(4, 30 / n); // degrés entre chaque carte
     const largeurDispo = Math.min(window.innerWidth - 24, 700)
     const offsetX      = Math.min(52, (largeurDispo - cardW *1.7) / (n - 1)); // px entre chaque carte
-    console.log({ n, cardW, largeurDispo, offsetX });
     joueur.main.forEach((carte, i) => {
         const milieu  = (n - 1) / 2;
         const delta   = i - milieu;
